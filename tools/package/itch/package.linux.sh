58c27a91ad5ba492e8caaf2839cd4ddde94e74ee
Andreas T Jonsson <mail@andreasjonsson.se>
2020-08-05 16:42:30 +0200

    Debugging build.

--- tools/package/itch/package.linux.sh
+++ tools/package/itch/package.linux.sh
@@ -6,14 +6,10 @@ mkdir -p $PACKAGE_DEST/bios
 
 exec tools/package/appimage/build.sh
 
-ls -al
-
-cp VirtualXT-x86_64.AppImage $PACKAGE_DEST
+cp VirtualXT-x86_64.AppImage $PACKAGE_DEST/
 #cp -r doc/manual $PACKAGE_DEST
 cp bios/pcxtbios.bin $PACKAGE_DEST/bios
-cp boot/freedos/freedos.img $PACKAGE_DEST
+cp boot/freedos/freedos.img $PACKAGE_DEST/
 cp tools/package/itch/itch.linux.toml $PACKAGE_DEST/.itch.toml
-
-ls -al $PACKAGE_DEST
 
 curl -L -o $PACKAGE_DEST/bios/ati_ega_wonder_800_plus.bin "https://github.com/BaRRaKudaRain/PCem-ROMs/raw/master/ATI%20EGA%20Wonder%20800%2B%20N1.00.BIN"
