32ffc2f09d53e541182e97d5395ed70051846314
Andreas T Jonsson <mail@andreasjonsson.se>
2020-08-05 19:49:01 +0200

    Test build

--- tools/package/itch/package.linux.sh
+++ tools/package/itch/package.linux.sh
@@ -6,10 +6,10 @@ mkdir -p $PACKAGE_DEST/bios
 
 ./tools/package/appimage/build.sh
 
-cp VirtualXT-x86_64.AppImage $PACKAGE_DEST/VirtualXT-x86_64.AppImage
+cp VirtualXT-x86_64.AppImage $PACKAGE_DEST/
 #cp -r doc/manual $PACKAGE_DEST
-cp bios/pcxtbios.bin $PACKAGE_DEST/bios/pcxtbios.bin
-cp boot/freedos/freedos.img $PACKAGE_DEST/freedos.img
+cp bios/pcxtbios.bin $PACKAGE_DEST/
+cp boot/freedos/freedos.img $PACKAGE_DEST/
 cp tools/package/itch/itch.linux.toml $PACKAGE_DEST/.itch.toml
 
 curl -L -o $PACKAGE_DEST/bios/ati_ega_wonder_800_plus.bin "https://github.com/BaRRaKudaRain/PCem-ROMs/raw/master/ATI%20EGA%20Wonder%20800%2B%20N1.00.BIN"
