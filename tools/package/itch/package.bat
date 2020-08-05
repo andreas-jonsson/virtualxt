8fb8e0030f9aa7c91a596f92eec552f3a48fb1fc
Andreas T Jonsson <mail@andreasjonsson.se>
2020-08-05 13:02:28 +0200

    Updated build script.

--- tools/package/itch/package.bat
+++ tools/package/itch/package.bat
@@ -2,11 +2,14 @@
 
 set PACKAGE_DEST="%TRAVIS_BUILD_DIR%\package\virtualxt"
 if exist %PACKAGE_DEST%\ rmdir /q /s %PACKAGE_DEST%
-mkdir %PACKAGE_DEST%
+mkdir %PACKAGE_DEST%\bios
 
 copy virtualxt.exe %PACKAGE_DEST%
 rem xcopy /S /Y /I doc\manual %PACKAGE_DEST%\manual
+copy bios\pcxtbios.bin %PACKAGE_DEST%\bios
 copy boot\freedos\freedos.img %PACKAGE_DEST%\freedos.img
 copy tools\package\itch\itch.windows.toml %PACKAGE_DEST%\.itch.toml
 
 copy SDL2-2.0.10\x86_64-w64-mingw32\bin\SDL2.dll %PACKAGE_DEST%
+
+wget -o %PACKAGE_DEST%\bios\ati_ega_wonder_800_plus.bin "https://github.com/BaRRaKudaRain/PCem-ROMs/raw/master/ATI%20EGA%20Wonder%20800%2B%20N1.00.BIN"
--- tools/package/itch/package.linux.sh
+++ tools/package/itch/package.linux.sh
@@ -2,11 +2,14 @@
 
 PACKAGE_DEST="$TRAVIS_BUILD_DIR/package/virtualxt"
 rm -rf $PACKAGE_DEST virtualxt-x86_64.AppImage
-mkdir -p $PACKAGE_DEST
+mkdir -p $PACKAGE_DEST/bios 
 
 exec tools/package/appimage/build.sh
 
 cp virtualxt-x86_64.AppImage $PACKAGE_DEST
 #cp -r doc/manual $PACKAGE_DEST
-cp boot/freedos/freedos.img $PACKAGE_DEST/freedos.img
+cp bios/pcxtbios.bin $PACKAGE_DEST/bios
+cp boot/freedos/freedos.img $PACKAGE_DEST
 cp tools/package/itch/itch.linux.toml $PACKAGE_DEST/.itch.toml
+
+wget -o $PACKAGE_DEST/bios/ati_ega_wonder_800_plus.bin "https://github.com/BaRRaKudaRain/PCem-ROMs/raw/master/ATI%20EGA%20Wonder%20800%2B%20N1.00.BIN"
