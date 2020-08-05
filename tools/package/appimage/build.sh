bcde3f6bfdebdbf958b2f1ef9b1d7931e40d9699
Andreas T Jonsson <mail@andreasjonsson.se>
2020-08-05 19:54:23 +0200

    Cleanup.

--- tools/package/appimage/build.sh
+++ tools/package/appimage/build.sh
@@ -6,8 +6,8 @@ rm -f VirtualXT-x86_64.AppImage
 mkdir -p $APP_DIR/lib/x86_64-linux-gnu
 
 # Should locate this in some way.
-cp /usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0 $APP_DIR/lib/x86_64-linux-gnu
-cp /usr/lib/x86_64-linux-gnu/libsndio.so.6.1 $APP_DIR/lib/x86_64-linux-gnu
+cp /usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0 $APP_DIR/lib/x86_64-linux-gnu/
+cp /usr/lib/x86_64-linux-gnu/libsndio.so.6.1 $APP_DIR/lib/x86_64-linux-gnu/
 
 cp virtualxt $APP_DIR/virtualxt
 cp doc/icon/icon.png $APP_DIR/virtualxt-icon.png
