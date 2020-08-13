60a192c97b546ab9f0af02c5b21f34b76e934d8f
Andreas T Jonsson <mail@andreasjonsson.se>
2020-08-13 11:19:41 +0200

    Updated OSX scripts

--- tools/package/appbundle/virtualxt-appbundle/Contents/MacOS/start.sh
+++ tools/package/appbundle/virtualxt-appbundle/Contents/MacOS/start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-osascript -e 'tell app "Terminal" to do script "\"'"$(cd "$(dirname "$0")" && pwd -P)/env.sh" "$@"'\""'
+HERE="$(cd "$(dirname "$0")" && pwd -P)"
+
+export VXT_DEFAULT_BIOS_PATH=${HERE}/bios/pcxtbios.bin
+export VXT_DEFAULT_VIDEO_BIOS_PATH=${HERE}/../../../bios/ati_ega_wonder_800_plus.bin
+export VXT_DEFAULT_FLOPPY_IMAGE=${HERE}/../../../boot/freedos.img
+
+exec "${HERE}/virtualxt" "$@"
--- tools/package/itch/package.osx.sh
+++ tools/package/itch/package.osx.sh
@@ -10,4 +10,12 @@ cp -r VirtualXT.app $PACKAGE_DEST
 cp boot/freedos.img $PACKAGE_DEST/boot/
 cp tools/package/itch/itch.osx.toml $PACKAGE_DEST/.itch.toml
 
+cat > $PACKAGE_DEST/Launch.sh << EOL
+#!/bin/bash
+HERE="$(cd "$(dirname "$0")" && pwd -P)"
+exec "${HERE}/VirtualXT.app/Contents/MacOS/start.sh" "$@"
+EOL
+
+chmod +x $PACKAGE_DEST/Launch.sh
+
 curl -L -o $PACKAGE_DEST/bios/ati_ega_wonder_800_plus.bin "https://github.com/BaRRaKudaRain/PCem-ROMs/raw/master/ATI%20EGA%20Wonder%20800%2B%20N1.00.BIN"
