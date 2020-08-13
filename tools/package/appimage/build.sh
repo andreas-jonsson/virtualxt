f7ef9750fcde59c29a0f95aaf8aa43f656db4cbe
Andreas T Jonsson <mail@andreasjonsson.se>
2020-08-13 15:46:26 +0200

    Moved to all static linking.

--- tools/package/appimage/build.sh
+++ tools/package/appimage/build.sh
@@ -4,11 +4,7 @@ APP_DIR="$TRAVIS_BUILD_DIR/tools/package/appimage/virtualxt-appimage-build"
 rm -rf $APP_DIR VirtualXT-x86_64.AppImage
 
 cp -r "$TRAVIS_BUILD_DIR/tools/package/appimage/virtualxt-appimage" $APP_DIR
-mkdir -p $APP_DIR/lib/x86_64-linux-gnu $APP_DIR/bios $APP_DIR/boot
-
-# Should locate this in some way.
-#cp /usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0 $APP_DIR/lib/x86_64-linux-gnu/
-#cp /usr/lib/x86_64-linux-gnu/libsndio.so.6.1 $APP_DIR/lib/x86_64-linux-gnu/
+mkdir -p $APP_DIR/bios $APP_DIR/boot
 
 cp virtualxt $APP_DIR/
 cp bios/pcxtbios.bin $APP_DIR/bios/
