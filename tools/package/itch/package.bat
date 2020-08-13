8b51d45002d57febc901e9eccef3cec1b87cf178
Andreas T Jonsson <mail@andreasjonsson.se>
2020-08-13 14:23:09 +0200

    Test build with static linking.

--- tools/package/itch/package.bat
+++ tools/package/itch/package.bat
@@ -10,6 +10,6 @@ copy bios\pcxtbios.bin %PACKAGE_DEST%\bios
 copy boot\freedos.img %PACKAGE_DEST%\boot
 copy tools\package\itch\itch.windows.toml %PACKAGE_DEST%\.itch.toml
 
-copy SDL2-2.0.10\x86_64-w64-mingw32\bin\SDL2.dll %PACKAGE_DEST%
+rem copy SDL2-2.0.10\x86_64-w64-mingw32\bin\SDL2.dll %PACKAGE_DEST%
 
 curl -L -o %PACKAGE_DEST%\bios\ati_ega_wonder_800_plus.bin "https://github.com/BaRRaKudaRain/PCem-ROMs/raw/master/ATI%%20EGA%%20Wonder%%20800%%2B%%20N1.00.BIN"
