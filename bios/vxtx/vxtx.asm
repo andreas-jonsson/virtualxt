d50892536d8d4058bfec9f76f96f7e1e94ed6575
Andreas T Jonsson <mail@andreasjonsson.se>
2020-12-08 12:10:36 +0100

    Changed license to zlib.

--- bios/vxtx/vxtx.asm
+++ bios/vxtx/vxtx.asm
@@ -1,18 +1,20 @@
-; VXTX - VirtualXT BIOS Extensions
-; Copyright (c) 2019-2020 Andreas T Jonsson (mail@andreasjonsson.se)
-;
-; This program is free software: you can redistribute it and/or modify
-; it under the terms of the GNU General Public License as published by
-; the Free Software Foundation, either version 3 of the License, or
-; (at your option) any later version.
-;
-; This program is distributed in the hope that it will be useful,
-; but WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-; GNU General Public License for more details.
-;
-; You should have received a copy of the GNU General Public License
-; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+; Copyright (c) 2019-2020 Andreas T Jonsson
+; 
+; This software is provided 'as-is', without any express or implied
+; warranty. In no event will the authors be held liable for any damages
+; arising from the use of this software.
+; 
+; Permission is granted to anyone to use this software for any purpose,
+; including commercial applications, and to alter it and redistribute it
+; freely, subject to the following restrictions:
+; 
+; 1. The origin of this software must not be misrepresented; you must not
+;    claim that you wrote the original software. If you use this software
+;    in a product, an acknowledgment in the product documentation would be
+;    appreciated but is not required.
+; 2. Altered source versions must be plainly marked as such, and must not be
+;    misrepresented as being the original software.
+; 3. This notice may not be removed or altered from any source distribution.
 
 cpu 8086
 org 0
@@ -85,7 +87,7 @@ INT_19_OFFSET  equ 0x64
 INT_19_SEGMENT equ 0x66
 
 db 'VXTX - VirtualXT BIOS Extensions', 0xA
-db 'This work is licensed under the GPL3 License.', 0
+db 'This work is licensed under the zlib license.', 0
 
 times 0x7FF-($-$$) db 0
 checksum db 0
