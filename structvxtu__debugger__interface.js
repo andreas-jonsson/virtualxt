9e5e6378a07e15898a876f37510384b1e15c3b65
andreas-jonsson <>
2022-02-14 12:38:14 +0000

    Deploying to gh-pages from @ andreas-jonsson/virtualxt@890d17a3ba01429419305a3a3993ae29f7022dc1 🚀

--- structvxtu__debugger__interface.js
+++ structvxtu__debugger__interface.js
@@ -1,7 +1,7 @@
 var structvxtu__debugger__interface =
 [
     [ "getline", "structvxtu__debugger__interface.html#af6b83143c9997e67a111620eee290652", null ],
-    [ "halt", "structvxtu__debugger__interface.html#a709a225534eea96ed808a3a80d9cf5e4", null ],
-    [ "pdisasm", "structvxtu__debugger__interface.html#aad89aa40312b4d5fb4b4d6555d532eab", null ],
-    [ "print", "structvxtu__debugger__interface.html#a83545da00cf23e3ab5565b05ae527888", null ]
+    [ "pdisasm", "structvxtu__debugger__interface.html#a465bfd88f886eb92da423e242f902da4", null ],
+    [ "print", "structvxtu__debugger__interface.html#a83545da00cf23e3ab5565b05ae527888", null ],
+    [ "trace", "structvxtu__debugger__interface.html#a87153b110cef3981f8a7bd1d8ebc599e", null ]
 ];
