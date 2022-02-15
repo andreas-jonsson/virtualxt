d116e8f0bb034bf617d4eeb393c2d37768c4f3c3
andreas-jonsson <>
2022-02-15 10:46:41 +0000

    Deploying to gh-pages from @ andreas-jonsson/virtualxt@079d9447f452c369a3c953fa5f87e68c034d7a79 🚀

--- annotated_dup.js
+++ annotated_dup.js
@@ -4,5 +4,6 @@ var annotated_dup =
     [ "vxt_pirepheral", "structvxt__pirepheral.html", "structvxt__pirepheral" ],
     [ "vxt_registers", "structvxt__registers.html", "structvxt__registers" ],
     [ "vxt_step", "structvxt__step.html", "structvxt__step" ],
+    [ "vxt_validator", "structvxt__validator.html", "structvxt__validator" ],
     [ "vxtu_debugger_interface", "structvxtu__debugger__interface.html", "structvxtu__debugger__interface" ]
 ];
---
+++ structvxt__validator.js
@@ -0,0 +1,11 @@
+var structvxt__validator =
+[
+    [ "begin", "structvxt__validator.html#a75b023ef12953c4233676a8a6a103c3d", null ],
+    [ "destroy", "structvxt__validator.html#ad7330ca3e01661093dacd903cb200191", null ],
+    [ "discard", "structvxt__validator.html#a4a34034f0eb44625904738ce6631d46c", null ],
+    [ "end", "structvxt__validator.html#aa04073405b42a398fa14a7745b83b596", null ],
+    [ "initialize", "structvxt__validator.html#aa1a8c709e1b30cbadc6a1fef60527bac", null ],
+    [ "read", "structvxt__validator.html#a60f241793fe03ca61851907823996cbf", null ],
+    [ "userdata", "structvxt__validator.html#afd0ffb02780e738d4c0a10ab833b7834", null ],
+    [ "write", "structvxt__validator.html#ae9e93488cf7ea8f85483bc15b7941deb", null ]
+];
