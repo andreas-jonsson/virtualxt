08830dc2f73a1df77dca2b5ba25e31b7146c0936
andreas-jonsson <>
2022-02-17 13:46:35 +0000

    Deploying to gh-pages from @ andreas-jonsson/virtualxt@0809fa9e5c55979972f4ef53ec2e644d4cf9b53b 🚀

--- structvxt__pirepheral.js
+++ structvxt__pirepheral.js
@@ -4,6 +4,7 @@ var structvxt__pirepheral =
     [ "install", "structvxt__pirepheral.html#a4fab5e6d319b14c6ba7fbf3605376e8d", null ],
     [ "io", "structvxt__pirepheral.html#ae56deb98874bb6d0463e2213be8137a0", null ],
     [ "name", "structvxt__pirepheral.html#abc41e5af11264b1449a4b5f1b088790d", null ],
+    [ "pclass", "structvxt__pirepheral.html#a7ad68d04ad11b8d4412ea93580fd54cb", null ],
     [ "pic", "structvxt__pirepheral.html#a2162a2eca268a28d83762f50dafdedb2", null ],
     [ "reset", "structvxt__pirepheral.html#afa7e4708f97b5d75131d15115b680c20", null ],
     [ "step", "structvxt__pirepheral.html#aedbdd9b0cbbf731bb83c4d92b0192f03", null ]
