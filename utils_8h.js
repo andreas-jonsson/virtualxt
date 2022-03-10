80ac66faac07a0f54276344032fa216c5d738f8f
andreas-jonsson <>
2022-03-10 10:01:56 +0000

    Deploying to gh-pages from @ andreas-jonsson/virtualxt@3bc73bc0c11bec51d8b2a06cc87ead4360748117 🚀

--- utils_8h.js
+++ utils_8h.js
@@ -2,9 +2,12 @@ var utils_8h =
 [
     [ "vxtu_debugger_interface", "structvxtu__debugger__interface.html", "structvxtu__debugger__interface" ],
     [ "vxtu_create_debugger", "utils_8h.html#af833b417e61bf32840f0a6317edfc14a", null ],
+    [ "vxtu_create_mda_device", "utils_8h.html#a27e75d30c4c82266a9240f387dc26e80", null ],
     [ "vxtu_create_memory_device", "utils_8h.html#a4a802db8897107d6f3a0d550e0ba8897", null ],
     [ "vxtu_create_pic", "utils_8h.html#a22e26c8605daa9acc3401f3fa345df8d", null ],
     [ "vxtu_debugger_interrupt", "utils_8h.html#a5101b920f9a48f0b72f4d8e5754f7570", null ],
+    [ "vxtu_mda_invalidate", "utils_8h.html#a68e92f29204c49d2594f2a183216006f", null ],
+    [ "vxtu_mda_traverse", "utils_8h.html#a6a5af433d5410b1d25ad10e0eb9113d7", null ],
     [ "vxtu_memory_device_fill", "utils_8h.html#a41eb9011a2e77e7ae3ec6020b6c72f66", null ],
     [ "vxtu_memory_internal_pointer", "utils_8h.html#a9ef35807c454f90245ff08d6c35f163b", null ]
 ];
