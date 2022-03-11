e542f954b0e0d92f74e5ab86ca3e5f3183c480f9
andreas-jonsson <>
2022-03-11 08:24:28 +0000

    Deploying to gh-pages from @ andreas-jonsson/virtualxt@730b0d753bba789e158e5dc1c6cd8abe94d4f1c5 🚀

--- utils_8h.js
+++ utils_8h.js
@@ -11,6 +11,7 @@ var utils_8h =
     [ "vxtu_create_mda_device", "utils_8h.html#a27e75d30c4c82266a9240f387dc26e80", null ],
     [ "vxtu_create_memory_device", "utils_8h.html#a4a802db8897107d6f3a0d550e0ba8897", null ],
     [ "vxtu_create_pic", "utils_8h.html#a22e26c8605daa9acc3401f3fa345df8d", null ],
+    [ "vxtu_create_ppi", "utils_8h.html#af3dbe1e783e5f598728ab55ae14fb930", null ],
     [ "vxtu_debugger_interrupt", "utils_8h.html#a5101b920f9a48f0b72f4d8e5754f7570", null ],
     [ "vxtu_mda_invalidate", "utils_8h.html#a68e92f29204c49d2594f2a183216006f", null ],
     [ "vxtu_mda_traverse", "utils_8h.html#a8604c410bfc57310be11504e41a49f86", null ],
