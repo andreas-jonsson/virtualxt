b2b092a1f93c4b5b2acec58638e0bc0d0e9fc877
andreas-jonsson <>
2022-04-12 11:24:03 +0000

    Deploying to gh-pages from @ andreas-jonsson/virtualxt@b477e14531a7fb878dde96dcf9984374516c88c3 🚀

--- structvxt__validator.js
+++ structvxt__validator.js
@@ -1,9 +1,9 @@
 var structvxt__validator =
 [
-    [ "begin", "structvxt__validator.html#ad0292dc3d42f75b29c251d599c4fcb74", null ],
+    [ "begin", "structvxt__validator.html#a49c4b3287abfcbc1500a436763278dc4", null ],
     [ "destroy", "structvxt__validator.html#ad7330ca3e01661093dacd903cb200191", null ],
     [ "discard", "structvxt__validator.html#a4a34034f0eb44625904738ce6631d46c", null ],
-    [ "end", "structvxt__validator.html#a842484a36e3f82f427669275c0fa2954", null ],
+    [ "end", "structvxt__validator.html#ac422a410927f5f11a548ace3bf58c70f", null ],
     [ "initialize", "structvxt__validator.html#aa1a8c709e1b30cbadc6a1fef60527bac", null ],
     [ "read", "structvxt__validator.html#a60f241793fe03ca61851907823996cbf", null ],
     [ "userdata", "structvxt__validator.html#afd0ffb02780e738d4c0a10ab833b7834", null ],
