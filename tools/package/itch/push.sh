6ab17388d0aa2b2b464a5c63a701f2efb75c68f6
Andreas T Jonsson <mail@andreasjonsson.se>
2022-05-25 12:28:34 +0200

    Build all branches

--- tools/package/itch/push.sh
+++ tools/package/itch/push.sh
@@ -3,5 +3,5 @@
 cd "${GITHUB_WORKSPACE}/package"
 butler validate virtualxt
 butler login
-butler push virtualxt phix/virtualxt:${RUNNER_OS}-${GITHUB_REF_NAME}-zig --userversion ${VXT_VERSION}-${GITHUB_RUN_ID}
+butler push virtualxt phix/virtualxt:${RUNNER_OS}-${GITHUB_REF_NAME} --userversion ${VXT_VERSION}-${GITHUB_RUN_ID}
 cd ..
