34ff8fdc99d33391f75e93ee73662db60e56e9db
Andreas T Jonsson <mail@andreasjonsson.se>
2022-11-09 10:31:39 +0100

    Updated web build

--- tools/package/itch/push-web.sh
+++ tools/package/itch/push-web.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-butler validate virtualxt.zip
 butler login
 butler push virtualxt.zip phix/virtualxt-web:${RUNNER_OS}-${GITHUB_REF_NAME} --userversion ${VXT_VERSION}-${GITHUB_RUN_ID}
