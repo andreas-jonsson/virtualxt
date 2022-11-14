0ae84d0d444776321d20726d5bc374d5ecb5318d
Andreas T Jonsson <mail@andreasjonsson.se>
2022-11-14 12:45:52 +0100

    Fixed linux package issue.

--- tools/package/appimage/build.sh
+++ tools/package/appimage/build.sh
@@ -6,8 +6,18 @@ rm -rf $APP_DIR VirtualXT-x86_64.AppImage
 cp -r "$GITHUB_WORKSPACE/tools/package/appimage/virtualxt-appimage" $APP_DIR
 mkdir -p $APP_DIR/usr/lib/x86_64-linux-gnu $APP_DIR/bios $APP_DIR/boot
 
+# SDL2 dependencies etc.
+cp /usr/lib/x86_64-linux-gnu/libSDL2-2.0.so* $APP_DIR/usr/lib/x86_64-linux-gnu/
+cp /usr/lib/x86_64-linux-gnu/libc.so* $APP_DIR/usr/lib/x86_64-linux-gnu/
+cp /usr/lib/x86_64-linux-gnu/libm.so* $APP_DIR/usr/lib/x86_64-linux-gnu/
+cp /usr/lib/x86_64-linux-gnu/ld-linux-x86-64.so* $APP_DIR/usr/lib/x86_64-linux-gnu/
+
 # Required for network support.
 cp /usr/lib/x86_64-linux-gnu/libpcap.so* $APP_DIR/usr/lib/x86_64-linux-gnu/
+cp /usr/lib/x86_64-linux-gnu/libibverbs.so* $APP_DIR/usr/lib/x86_64-linux-gnu/
+cp /usr/lib/x86_64-linux-gnu/libnl-route-3.so* $APP_DIR/usr/lib/x86_64-linux-gnu/
+cp /usr/lib/x86_64-linux-gnu/libnl-3.so* $APP_DIR/usr/lib/x86_64-linux-gnu/
+cp /usr/lib/x86_64-linux-gnu/libgcc_s.so* $APP_DIR/usr/lib/x86_64-linux-gnu/
 
 cp build/bin/virtualxt $APP_DIR/
 cp bios/pcxtbios.bin $APP_DIR/bios/
