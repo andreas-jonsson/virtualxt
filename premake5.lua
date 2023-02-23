0a4ef774e94d57688e7cb88735ed662898b48bd3
Andreas T Jonsson <mail@andreasjonsson.se>
2023-02-23 11:37:45 +0100

    Updated SDL path

--- premake5.lua
+++ premake5.lua
@@ -1,34 +1,72 @@
 newoption {
     trigger = "sdl-path",
     value = "PATH",
+    default = "PATH",
     description = "Path to SDL2 headers and libs"
 }
 
+newoption {
+    trigger = "pcap",
+    description = "Enable network support by linking with pcap"
+}
+
+newoption {
+    trigger = "validator",
+    description = "Enable the PI8088 hardware validator"
+}
+
+newoption {
+    trigger = "cpu",
+    description = "Select CPU support",
+    default = "8088",
+    allowed = {
+        {"8088", "Intel 8088"},
+        {"V20",  "NEC V20"},
+        {"286",  "Intel 80286"}
+    }
+}
+
 workspace "virtualxt"
-    configurations { "Release", "Debug" }
+    configurations { "release", "debug" }
     location "premake"
     language "C"
     cdialect "C11"
+
+    filter "configurations:debug"
+        defines "DEBUG"
+        symbols "On"
+
+    filter "configurations:release"
+        defines "NDEBUG"
+        optimize "On"
+
+    filter "options:cpu=V20"
+        defines "VXT_CPU_V20"
+
+    filter "options:cpu=286"
+        defines "VXT_CPU_286"
+
+    filter "options:validator"
+        defines "PI8088"
+        links "gpiod"
+
+    filter "options:pcap"
+        defines "VXTP_NETWORK"
+        links "pcap"
 
     filter { "action:gmake" }
         buildoptions { "-pedantic", "-Wall", "-Wextra", "-Werror", "-Wno-implicit-fallthrough", "-Wno-unused-result" }
 
     project "inih"
         kind "StaticLib"
-        defines "NDEBUG"
-        optimize "On"
         files { "lib/inih/ini.h", "lib/inih/ini.c" }
 
     project "microui"
         kind "StaticLib"
-        defines "NDEBUG"
-        optimize "On"
         files { "lib/microui/src/microui.h", "lib/microui/src/microui.c" }
 
     project "nuked-opl3"
         kind "StaticLib"
-        defines "NDEBUG"
-        optimize "On"
         files { "lib/nuked-opl3/opl3.h", "lib/nuked-opl3/opl3.c" }
 
     project "libvxt"
@@ -55,16 +93,11 @@ workspace "virtualxt"
         files { "lib/vxtp/*.h", "lib/vxtp/*.c" }
         includedirs { "ib/vxtp", "lib/vxt/include", "lib/nuked-opl3"  }
 
+        filter "not options:pcap"
+            removefiles "lib/vxtp/network.c"
+
         filter "action:gmake"
             buildoptions { "-Wno-format-truncation", "-Wno-stringop-truncation", "-Wno-stringop-overflow" }
-
-        filter "configurations:Debug"
-            defines "DEBUG"
-            symbols "On"
-
-        filter "configurations:Release"
-            defines "NDEBUG"
-            optimize "On"
 
     project "sdl2-frontend"
         kind "ConsoleApp"
@@ -73,20 +106,18 @@ workspace "virtualxt"
 
         files { "front/sdl/*.h", "front/sdl/*.c" }
 
-        links { "SDL2", "libvxt", "libvxtp", "inih", "microui", "nuked-opl3" }
+        links { "libvxt", "libvxtp", "inih", "microui", "nuked-opl3" }
         includedirs { "lib/vxt/include", "lib/vxtp", "lib/inih", "lib/microui/src" }
 
-        filter "options:sdl-path"
+        filter "options:validator"
+            files "tools/validator/pi8088/pi8088.c"
+
+        filter "not options:sdl-path=PATH"
             includedirs { _OPTIONS["sdl-path"] .. "/include" }
-            libdirs { _OPTIONS["sdl-path"] .. "/lib" }
-
-        filter "configurations:Debug"
-            defines "DEBUG"
-            symbols "On"
-
-        filter "configurations:Release"
-            defines "NDEBUG"
-            optimize "On"
+            links { _OPTIONS["sdl-path"] .. "/lib/SDL2" }
+        
+        filter "options:sdl-path=PATH"
+            links "SDL2"
 
         filter "action:gmake"
             buildoptions { "-Wno-unused-variable", "-Wno-unused-parameter", "-Wno-maybe-uninitialized", "-Wno-stringop-truncation" }
