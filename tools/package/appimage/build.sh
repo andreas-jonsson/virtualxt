68f4c5b1fa0e9d8430c9eda310862bd368e02ec1
Andreas T Jonsson <mail@andreasjonsson.se>
2023-03-07 09:40:07 +0100

    Debugging appimage build

--- tools/package/appimage/build.sh
+++ tools/package/appimage/build.sh
@@ -10,6 +10,8 @@ mkdir -p $APP_DIR/usr/lib/x86_64-linux-gnu $APP_DIR/lib64 $APP_DIR/bios $APP_DIR
 DST="$APP_DIR/usr/lib/x86_64-linux-gnu"
 SRC="/usr/lib/x86_64-linux-gnu"
 
+ls -al "$GITHUB_WORKSPACE/sdl_bin/lib/"
+
 cp -a "$GITHUB_WORKSPACE/sdl_bin/lib/libSDL2*.so*" $APP_DIR/usr/lib
 
 #cp -a $SRC/libc.so* $DST
