e1b96fc8982336432abf450f7b5143e4ac553bce
Andreas T Jonsson <mail@andreasjonsson.se>
2023-03-20 10:12:01 +0100

    Fixed link issue on wasm

--- premake5.lua
+++ premake5.lua
@@ -164,8 +164,8 @@ workspace "virtualxt"
         }
 
         buildoptions { "--target=wasm32", "-mbulk-memory", "-flto" }
-        linkoptions { "--target=wasm32", "-nostdlib", "-Wl,--allow-undefined", "--lto-O3", ",--no-entry", ",--export-all", ",--import-memory" }
-        linkoptions { "-Wl,--initial-memory=" .. tostring(memory.initial), ",--max-memory=" .. tostring(memory.max), ",--global-base=" .. tostring(memory.base) }
+        linkoptions { "--target=wasm32", "-nostdlib", "-Wl,--allow-undefined", "-Wl,--lto-O3", "-Wl,--no-entry", "-Wl,--export-all", "-Wl,--import-memory" }
+        linkoptions { "-Wl,--initial-memory=" .. tostring(memory.initial), "-Wl,--max-memory=" .. tostring(memory.max), "-Wl,--global-base=" .. tostring(memory.base) }
 
         postbuildcommands {
             "{COPYFILE} front/web/index.html build/web/",
