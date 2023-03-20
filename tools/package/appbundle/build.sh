112a3e4194293d170cf17d08de099c1e598f2119
Andreas T Jonsson <mail@andreasjonsson.se>
2023-03-20 10:57:38 +0100

    Run package builder on OSX

--- tools/package/appbundle/build.sh
+++ tools/package/appbundle/build.sh
@@ -16,4 +16,6 @@ cp tools/icon/icon.icns $APP_DIR/Contents/Resources/
 
 YEAR=$(date +%Y)
 rpl e14f19fc-199d-4fb9-b334-aed07b29a113 $YEAR $APP_DIR/Contents/Info.plist
-rpl e34f19fc-199d-4fb9-b334-aed07b29a173 $FULL_VERSION $APP_DIR/Contents/Info.plist
+
+VERSION=${VXT_VERSION}-${GITHUB_RUN_ID}
+rpl e34f19fc-199d-4fb9-b334-aed07b29a173 $VERSION $APP_DIR/Contents/Info.plist
