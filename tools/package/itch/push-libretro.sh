80217f0cbf9f2138b9829d4490fc7721c13a396c
Andreas T Jonsson <mail@andreasjonsson.se>
2023-03-23 10:02:54 +0100

    Minor build tweeks

--- tools/package/itch/push-libretro.sh
+++ tools/package/itch/push-libretro.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 butler login
-butler push package/virtualxt phix/virtualxt:libretro-${RUNNER_OS}-${GITHUB_REF_NAME} --userversion ${VXT_VERSION}-${GITHUB_RUN_ID}
+butler push package phix/virtualxt:libretro-${RUNNER_OS}-${GITHUB_REF_NAME} --userversion ${VXT_VERSION}-${GITHUB_RUN_ID}
