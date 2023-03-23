fd438f5a47e63d7adc202ebb5c91fb5d46fcfa92
Andreas T Jonsson <mail@andreasjonsson.se>
2023-03-23 09:56:40 +0100

    Skip zip package

--- tools/package/itch/push-libretro.sh
+++ tools/package/itch/push-libretro.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 butler login
-butler push virtualxt.zip phix/virtualxt:libretro-${RUNNER_OS}-${GITHUB_REF_NAME} --userversion ${VXT_VERSION}-${GITHUB_RUN_ID}
+butler push package/virtualxt phix/virtualxt:libretro-${RUNNER_OS}-${GITHUB_REF_NAME} --userversion ${VXT_VERSION}-${GITHUB_RUN_ID}
