e3bbc804ae9fdf2244b2d25f590c2a17cf513e29
Andreas T Jonsson <mail@andreasjonsson.se>
2023-03-23 09:36:21 +0100

    Try windows builder.

--- tools/package/itch/push-libretro.sh
+++ tools/package/itch/push-libretro.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 butler login
-butler push virtualxt.zip phix/virtualxt:libretro-${GITHUB_REF_NAME} --userversion ${VXT_VERSION}-${GITHUB_RUN_ID}
+butler push virtualxt.zip phix/virtualxt:libretro-${RUNNER_OS}-${GITHUB_REF_NAME} --userversion ${VXT_VERSION}-${GITHUB_RUN_ID}
