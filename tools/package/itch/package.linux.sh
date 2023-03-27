387df00107c59af3b210c91516eee95d7fd1187d
Andreas T Jonsson <mail@andreasjonsson.se>
2023-03-27 22:20:55 +0200

    Need to copy before creating image

--- tools/package/itch/package.linux.sh
+++ tools/package/itch/package.linux.sh
@@ -4,9 +4,10 @@ PACKAGE_DEST="$GITHUB_WORKSPACE/package/virtualxt"
 rm -rf $PACKAGE_DEST
 mkdir -p $PACKAGE_DEST
 
-./tools/package/appimage/build.sh
+mkdir -p "$GITHUB_WORKSPACE/tools/package/appimage/virtualxt-appimage/usr/lib/"
+cp -a "$GITHUB_WORKSPACE/sdl_bin/lib/libSDL2"*.so* "$GITHUB_WORKSPACE/tools/package/appimage/virtualxt-appimage/usr/lib/"
 
-cp -a "$GITHUB_WORKSPACE/sdl_bin/lib/libSDL2"*.so* "$GITHUB_WORKSPACE/tools/package/appimage/virtualxt-appimage-build/usr/lib/"
+./tools/package/appimage/build.sh
 
 cp VirtualXT-x86_64.AppImage $PACKAGE_DEST/
 cp tools/package/itch/itch.linux.toml $PACKAGE_DEST/.itch.toml
