00d4ec499aa1a3bdc67cdfadee6205a74458558e
Andreas T Jonsson <mail@andreasjonsson.se>
2023-05-02 15:32:14 +0200

    Fixed build issue.

--- modules/isa/premake5.lua
+++ modules/isa/premake5.lua
@@ -1,3 +1,5 @@
-defines { "_POSIX_C_SOURCE=200809L", "FASYNC=O_ASYNC" }
-files { "isa.c", "ch36x/ch36x_lib.h", "ch36x/ch36x_lib.c" }
-buildoptions { "-Wno-unused-parameter", "-Wno-unused-variable", "-Wno-type-limits" }
+files "isa.c"
+filter "system:linux"
+    defines { "_POSIX_C_SOURCE=200809L", "FASYNC=O_ASYNC" }
+    files { "ch36x/ch36x_lib.h", "ch36x/ch36x_lib.c" }
+    buildoptions { "-Wno-unused-parameter", "-Wno-unused-variable", "-Wno-type-limits" }
