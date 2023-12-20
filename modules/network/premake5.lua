5799d849fba488b5ba9787e04275bbbfea75b924
Andreas 'phix' Jonsson <mail@andreasjonsson.se>
2023-12-20 11:22:33 +0100

    Network module works on static builds

--- modules/network/premake5.lua
+++ modules/network/premake5.lua
@@ -1,9 +1,13 @@
 files "network.c"
 
-filter "not system:windows"
-    links "pcap"
-
 filter "system:windows"
     includedirs "../../tools/npcap/sdk/Include"
     defines "_WINSOCK_DEPRECATED_NO_WARNINGS"
-    links { "Ws2_32", "../../tools/npcap/sdk/Lib/x64/wpcap" }
+
+module_link_callback(function()
+	filter "not system:windows"
+		links "pcap"
+
+	filter "system:windows"
+		links { "Ws2_32", "../../tools/npcap/sdk/Lib/x64/wpcap" }
+end)
