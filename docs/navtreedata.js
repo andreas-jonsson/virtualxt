baf8268fd52b36485751858dfd5589f77fbff156
andreas-jonsson <>
2024-10-11 14:40:35 +0000

    Deploying to gh-pages from @ andreas-jonsson/virtualxt@9ceb62d32340e541c57d45f53de2cf83f65fd036 🚀

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -50,7 +50,7 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "annotated.html",
-"vxtu_8h.html#aa29e455bcd0f1a145978cdecce90e98fa5a14096c4e20c0d603b191612ec56941"
+"vxtu_8h.html#aa29e455bcd0f1a145978cdecce90e98fa6538333b08b10af255b9575956f6b690"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- docs/structvxt__step.js
+++ docs/structvxt__step.js
@@ -4,5 +4,6 @@ var structvxt__step =
     [ "err", "structvxt__step.html#a5cebf34f7718b7c1e84f667151e41db8", null ],
     [ "halted", "structvxt__step.html#a99213d1f6d8edb0a1b1c9e073075e414", null ],
     [ "int28", "structvxt__step.html#ae950789d64979014b3938266e07f922e", null ],
+    [ "interrupt", "structvxt__step.html#abeedec982d6cd374d8f100e3688de085", null ],
     [ "invalid", "structvxt__step.html#a287d7f5cafc29863a809d81fc80d55b3", null ]
 ];
