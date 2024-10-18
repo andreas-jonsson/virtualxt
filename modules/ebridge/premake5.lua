90a161f727948f01673ca80334e58ee61f43147e
Andreas T Jonsson <mail@andreasjonsson.se>
2024-10-18 13:28:25 +0200

    Fixed link issue on Windows

--- modules/ebridge/premake5.lua
+++ modules/ebridge/premake5.lua
@@ -1,4 +1,8 @@
 files "ebridge.c"
 filter "system:windows"
 	defines "_WINSOCK_DEPRECATED_NO_WARNINGS"
-	links "Ws2_32"
+	
+module_link_callback(function()
+	filter "system:windows"
+		links "Ws2_32"
+end)
